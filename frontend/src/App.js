fad6c672fbf0c79361db04636a1edb5b55b30bb6
yashuvaishnav <yashurishu22@gmail.com>
2023-11-01 22:12:44 +0530

    Done Checkout page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import { Checkout } from './Checkout';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Yash</h1>
+      <Checkout />
     </div>
   );
 }
