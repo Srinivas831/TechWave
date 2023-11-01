331744e9cb8e733b627b000e3c137339f80167ab
Its-Arup <aruppaul1650@gmail.com>
2023-11-01 22:20:56 +0530

    home page navbar nad foorter done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import AllRoutes from './Components/AllRoutes';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <AllRoutes/>
+      <Footer/>
     </div>
   );
 }
