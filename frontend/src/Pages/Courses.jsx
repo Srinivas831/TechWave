d2e6189ec24e7ae2fe50b74ebbc51edeca725600
unknown <makarand.gijam22@gmail.com>
2023-11-02 22:40:13 +0530

    courses page in progress

--- frontend/src/Pages/Courses.jsx
+++ frontend/src/Pages/Courses.jsx
@@ -1,7 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { CourseCard } from "../Components/CourseCard";
+
 
 export const Courses = () => {
+  const [courses, setCourses] = useState([]);
+
+  useEffect(() => {
+    fetch("https://techwave-test.onrender.com/courses")
+      .then((res) => res.json())
+      .then((res) => {
+        console.log(res);
+        setCourses(res);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   return (
-    <div>Courses</div>
-  )
-}
+    <div>
+      <h1>Welcome to the Courses</h1>
+      <div>
+        {courses.length > 0 &&
+          courses.map((course) => (
+            <CourseCard key={course.id} course={course} />
+          ))}
+      </div>
+    </div>
+  );
+};
