8797e879ce2b68a08dd8c1db2188c9a3ecf0127d
Srinivas831 <hsrinivas802@gmail.com>
2023-11-02 22:52:42 +0530

    small change done in app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,14 @@
 import AllRoutes from './Components/AllRoutes';
 import Footer from './Components/Footer';
 import Navbar from './Components/Navbar';
-import {Checkout} from "./Pages/Checkout";
+
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/>
+      <Navbar/>
       <AllRoutes/>
-      <Footer/> */}
-      <Checkout />
+      <Footer/>
+      {/* <Checkout /> */}
     </div>
   )
 }
