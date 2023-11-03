2df026c4ee3eaece7fcd45aef3c5b56ad62ffe19
Srinivas831 <hsrinivas802@gmail.com>
2023-11-03 21:57:59 +0530

    done with single page

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use("/users",userRouter);
 app.use("/courses",productRouter);
 
-app.get("/",()=>{
+app.get("/",(req,res)=>{
     res.send("server is working");
 })
 
--- frontend/src/Components/AllRoutes.jsx
+++ frontend/src/Components/AllRoutes.jsx
@@ -3,12 +3,16 @@ import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/Home";
 import SingleCoursePage from "./SingleCoursePage";
 import { Checkout } from "../Pages/Checkout";
+import Signup from "../Pages/Signup";
+import Login from "../Pages/Login";
 function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/singlecourse" element={<SingleCoursePage />} />
       <Route path="/checkout" element={<Checkout />}/>
+      <Route path="/signup" element={<Signup />}/>
+      <Route path="/login" element={<Login />}/>
     </Routes>
   );
 }
