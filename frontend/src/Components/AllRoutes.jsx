7dc07de3167b2fcc83709dc0403985f0f2095fa1
Its-Arup <aruppaul1650@gmail.com>
2023-11-05 22:51:05 +0530

    cart page myLearning page done

--- frontend/src/Components/AllRoutes.jsx
+++ frontend/src/Components/AllRoutes.jsx
@@ -6,17 +6,20 @@ import { Checkout } from "../Pages/Checkout";
 import Signup from "../Pages/Signup";
 import Login from "../Pages/Login";
 import { Blog } from "../Pages/Blog";
+import Cart from "../Pages/Cart";
+import MyLearning from "../Pages/MyLearning";
 
 function AllRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/singlecourse" element={<SingleCoursePage />} />
-
       <Route path="/checkout" element={<Checkout />} />
       <Route path="/blogs" element={<Blog />} />
-      <Route path="/signup" element={<Signup />}/>
-      <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/mylearning" element={<MyLearning />} />
     </Routes>
   );
 }
