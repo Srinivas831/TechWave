344ee93e028d63d303c8326ae4a785f9ded95c51
Its-Arup <aruppaul1650@gmail.com>
2023-11-12 16:29:29 +0530

    api connected

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,5 @@
 // export const url="http://localhost:8080";
-export const url="https://calm-gold-slug-toga.cyclic.app/";
+// export const url="https://calm-gold-slug-toga.cyclic.app/";
 // export const url="https://techwave-backend.onrender.com/";
 // export const url="https://techwave-production-8e65.up.railway.app/";
+export const url="https://tech-wave-backend-server.onrender.com";
