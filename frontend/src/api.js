5616bb376f0e9867eca0e6da3a8942e965c1574f
Its-Arup <aruppaul1650@gmail.com>
2023-11-12 16:06:08 +0530

    done

---
+++ frontend/src/api.js
@@ -0,0 +1,4 @@
+// export const url="http://localhost:8080";
+export const url="https://calm-gold-slug-toga.cyclic.app/";
+// export const url="https://techwave-backend.onrender.com/";
+// export const url="https://techwave-production-8e65.up.railway.app/";
