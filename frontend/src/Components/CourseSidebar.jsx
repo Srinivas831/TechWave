81bfd1ba04f2941fb4b54243e7a0f6429db6b452
unknown <makarand.gijam22@gmail.com>
2023-12-14 19:10:14 +0530

    slider css done

--- frontend/src/Components/CourseSidebar.jsx
+++ frontend/src/Components/CourseSidebar.jsx
@@ -83,38 +83,6 @@ export const CourseSidebar = ({ handleFilters, handleSort }) => (
         </div>
       </div>
     </div>
-
-    <div className={styles.checkboxWrapper}>
-      <div className={styles.cbx}>
-        <div>
-          <input type="checkbox" id="cbx-12" name="order" value={"asc"}
-            onChange={handleSort} />
-          <label for="cbx-12">ASC</label>
-        </div>
-
-        <input type="checkbox" id="cbx-34" name="order" value={"desc"}
-          onChange={handleSort} />
-        <label for="cbx-34">DESC</label>
-
-        <input type="checkbox" id="cbx-56" name="order" value={""}
-          onChange={handleSort} />
-        <label for="cbx-54">RESET</label>
-        {/* <span>Low To High</span> */}
-        <svg fill="none" viewBox="0 0 15 14" height="14" width="15">
-          <path d="M2 8.36364L6.23077 12L13 2"></path>
-        </svg>
-      </div>
-
-      <svg version="1.1" xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <filter id="goo-12">
-            <feGaussianBlur result="blur" stdDeviation="4" in="SourceGraphic"></feGaussianBlur>
-            <feColorMatrix result="goo-12" values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 22 -7" mode="matrix" in="blur"></feColorMatrix>
-            <feBlend in2="goo-12" in="SourceGraphic"></feBlend>
-          </filter>
-        </defs>
-      </svg>
-    </div>
   </div>
 );
 
